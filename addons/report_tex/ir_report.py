d592376803bc611c16aa85a7de24ef42bd10dee7
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-02-23 11:40:09 -0300

    [FIX] Ignore ropeproject directory. [FIX] v8.0 report selection

--- addons/report_tex/ir_report.py
+++ addons/report_tex/ir_report.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api, _, report, addons
+from openerp import models, fields, report, addons
 from latex_report import LatexParser
+
 
 class ir_actions_report_xml(models.Model):
     _inherit = 'ir.actions.report.xml'
 
-    def __init__(self,*args,**kwargs):
-        super(models.Model, self).__init__(*args,**kwargs)
-        selection=self._fields["report_type"].selection
-        if "latex" not in dict(selection):
-            selection.append(("latex","Latex"))
+    report_type = fields.Selection(selection_add=[("tex", "Tex")])
 
     def _lookup_report(self, cr, name):
         """
@@ -32,16 +29,18 @@ class ir_actions_report_xml(models.Model):
             if r:
                 if r['parser']:
                     parser = operator.attrgetter(r['parser'])(addons)
-                    kwargs = { 'parser': parser }
+                    kwargs = {'parser': parser}
                 else:
                     kwargs = {}
 
                 new_report = LatexParser('report.'+r['report_name'],
-                    r['model'], opj('addons',r['report_file'] or '/'),
-                    header=r['header'], register=False, **kwargs)
+                                         r['model'],
+                                         opj('addons', r['report_file'] or '/'),
+                                         header=r['header'],
+                                         register=False, **kwargs)
             else:
                 new_report = None
- 
+
         if new_report:
             return new_report
         else:
